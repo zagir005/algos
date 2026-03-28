f2968423f03a05a1eb44afcd69e2137e862dff9c
zagir <gadzimirzoevz05@gmail.com>
2026-03-28 23:47:19 +0300

    509 Task Solved

---
+++ src/everyday/509. Fibonacci Number.kt
@@ -0,0 +1,20 @@
+package everyday
+
+/*
+* Algorithm complexity: O(n)
+* Memory complexity: O(1)
+ */
+private fun fib(n: Int): Int {
+    if (n <= 1) return n
+
+    var prev = 0
+    var curr = 1
+
+    for (i in 2..n) {
+        val next = prev + curr
+        prev = curr
+        curr = next
+    }
+
+    return curr
+}
