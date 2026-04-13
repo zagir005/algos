2e6f20e7de3e2f79fe3e4392904c5ae64d31ba8d
zagir <gadzimirzoevz05@gmail.com>
2026-04-13 13:49:04 +0300

    Q3. Max Consecutive Ones solved

---
+++ src/lc/array1/Q3. Max Consecutive Ones.kt
@@ -0,0 +1,21 @@
+package lc.array1
+
+/**
+ * Просто подсчет стрика единичек в бинарном массиве. Нужно найти максимальный стрик (идущие друг за другом единички)
+ * мое решение оказалось медленее лучшего из-за доп. проверки после цикла
+ * можно избежать если в if чекать именно i==1
+ */
+private fun findMaxConsecutiveOnes(nums: IntArray): Int {
+    var maxStreak = 0
+    var currStreak = 0
+    for(i in nums){
+        if(i == 0){
+            if(currStreak > maxStreak) maxStreak = currStreak
+            currStreak = 0
+        }
+        else
+            currStreak++
+    }
+    if(currStreak > maxStreak) maxStreak = currStreak
+    return maxStreak
+}
