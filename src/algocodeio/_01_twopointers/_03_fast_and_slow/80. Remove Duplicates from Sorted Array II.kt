2c6a359c722c85b974e7d8454fbecbebb9f8bd64
zagir <gadzimirzoevz05@gmail.com>
2026-04-16 12:13:13 +0300

    80. Remove Duplicates from Sorted Array II - Solved

---
+++ src/algocodeio/_01_twopointers/_03_fast_and_slow/80. Remove Duplicates from Sorted Array II.kt
@@ -0,0 +1,42 @@
+package algocodeio._01_twopointers._03_fast_and_slow
+
+import kotlin.math.min
+
+/**
+    two pointers, fast and slow
+    т.к. в kotlin удалить из IntArray элементы нельзя, нужно перенести лишние в конец
+    lc сам глянет первые k нужных элементов
+
+    задача тяжело пошла, без подсказки и блокнота не получалось решить
+    ясно что это быстрый и медленный указатель, но как условие выстроить что бы в нужный
+    момент элементы местами менять непонятно
+
+    в итоге суть в том, что бы проверять элемент по индексу sl - 2
+
+    O(n)
+    O(1)
+    1ч. 13м
+ */
+fun removeDuplicates(nums: IntArray): Int {
+    var sl = 2
+    var fs = 2
+
+    while(fs <= nums.lastIndex){
+        val fsNum = nums[fs]
+        val slNum = nums[sl]
+        if(fsNum != nums[sl-2]){
+            val temp = fsNum
+            nums[fs] = slNum
+            nums[sl] = temp
+            sl++
+        }
+        fs++
+    }
+    return min(sl, nums.size)
+}
+
+fun main(){
+    removeDuplicates(
+        nums = intArrayOf(1,1,1,2,2,3)
+    )
+}
