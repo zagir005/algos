425c3ae63457da9d234195f2a9a1b7a5c849bbac
zagir <gadzimirzoevz05@gmail.com>
2026-04-22 22:49:14 +0300

    1464. Maximum Product of Two Elements in an Array - Solved

---
+++ src/algocodeio/_03_arrays_and_matrix/1464. Maximum Product of Two Elements in an Array.kt
@@ -0,0 +1,22 @@
+package algocodeio._03_arrays_and_matrix
+
+/**
+крч идея в том, что aP у нас будет идти впереди и указывать на наибольшее число,
+а bP просто подбирать за ним. Если найдем число равное aP, то запишем в bP
+
+по началу обозначал индексами, в итоге убрал, прост напрямую число сохраняю
+ */
+private fun maxProduct(nums: IntArray): Int {
+    var max = 0
+    var min = 0
+
+    for (num in nums)
+        if (num > max) {
+            min = max
+            max = num
+        } else if (num > min) {
+            min = num
+        }
+
+    return (max - 1) * (min - 1)
+}
